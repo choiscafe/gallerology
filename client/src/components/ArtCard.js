1019afc0b6dfbd11d1673e6876b0b5ccf3d145f7
Chohye Han <choiscafe@gmail.com>
2022-10-26 14:48:09 -0400

    li changed to ul

--- client/src/components/ArtCard.js
+++ client/src/components/ArtCard.js
@@ -3,7 +3,7 @@ function ArtCard({ artwork }) {
   const {image, title, year, gallery, exhibition, notes, seenDate, artist_id} = artwork
 
   return (
-    <li className="art-card">
+    <ul className="art-card">
       <img src={image} alt={title} />
       <h4>{title}</h4>
       <p>{year}</p>
@@ -12,7 +12,7 @@ function ArtCard({ artwork }) {
       <p>{notes}</p>
       <p>{seenDate}</p>
       <p>{artist_id}</p>
-    </li>
+    </ul>
   )
 }
 export default ArtCard
