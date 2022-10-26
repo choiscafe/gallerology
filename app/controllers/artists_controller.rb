5c3c5d8fd901afe027c5e105ca4b1af38f8a972c
Chohye Han <choiscafe@gmail.com>
2022-10-25 20:23:24 -0400

    ArtistController Update added

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,6 +4,15 @@ class ArtistsController < ApplicationController
     render json: artists
   end
 
+  def show
+    artist = Artist.find_by(id: params[:id])
+    if artist
+      render json: artist, status: :ok
+    else
+      render json: { error: "Artist not found" }, status: :not_found
+    end
+  end
+
   def create
     artist = Artist.create(artist_params)
     if artist.valid?
@@ -11,6 +20,20 @@ class ArtistsController < ApplicationController
     else
       render json: { errors: artist.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    artist = Artist.find_by(id: params[:id])
+    if artist
+      artist.update(artist_params)
+      if artist.valid?
+        render json: artist, status: :accepted
+      else
+        render json: { errors: artist.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Artist not found" }, status: :not_found
+    end 
   end
 
   def artist_params
